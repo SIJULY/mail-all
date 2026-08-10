6d96b899f915595195b950c69af55ec237422ef0
SIJULY <sijuly@outlook.com>
2026-08-10 09:40:46 +0800

    refactor: optimize ui layout, update settings page and remove obsolete components

--- test_req.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import urllib.error
-import http.cookiejar
-
-cj = http.cookiejar.CookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-
-# login first
-data = b"username=sijuly@outlook.com&password=testpassword" # wait I don't know the password.
